8437c45bc25ba8a1fdc2e13ac1db4d2d70473c52
Martin <MartinGlyer@gmail.com>
2018-07-24 13:27:28 -0400

    to cut down on API pings for now, requesting only 1 pet

--- server/contollers.js
+++ server/contollers.js
@@ -15,11 +15,12 @@ module.exports = {
         get: async (req, res) => {
             console.log('in the server controllers.')
             let pet1 = await axios.get(`http://api.petfinder.com/pet.getRandom?key=${API_KEY}&output=basic&format=json`)
-            let pet2 = await axios.get(`http://api.petfinder.com/pet.getRandom?key=${API_KEY}&output=basic&format=json`)
-            let pet3 = await axios.get(`http://api.petfinder.com/pet.getRandom?key=${API_KEY}&output=basic&format=json`)
-            let pet4 = await axios.get(`http://api.petfinder.com/pet.getRandom?key=${API_KEY}&output=basic&format=json`)
-            let pet5 = await axios.get(`http://api.petfinder.com/pet.getRandom?key=${API_KEY}&output=basic&format=json`)
-            res.send([pet1.data, pet2.data, pet3.data, pet4.data, pet5.data])
+            // let pet2 = await axios.get(`http://api.petfinder.com/pet.getRandom?key=${API_KEY}&output=basic&format=json`)
+            // let pet3 = await axios.get(`http://api.petfinder.com/pet.getRandom?key=${API_KEY}&output=basic&format=json`)
+            // let pet4 = await axios.get(`http://api.petfinder.com/pet.getRandom?key=${API_KEY}&output=basic&format=json`)
+            // let pet5 = await axios.get(`http://api.petfinder.com/pet.getRandom?key=${API_KEY}&output=basic&format=json`)
+            // res.send([pet1.data, pet2.data, pet3.data, pet4.data, pet5.data])
+            res.send(pet1.data)
         }
     },
 
