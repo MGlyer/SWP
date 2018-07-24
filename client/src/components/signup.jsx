29615eae52541723a3081ab70f1b71acd79345e5
Martin <MartinGlyer@gmail.com>
2018-07-24 14:14:05 -0400

    creates signup method, client side

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -35,3 +35,5 @@ class Signup extends React.Component {
         )
     }
 }
+
+export default Signup
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,18 +4,28 @@ import axios from 'axios'
 import {BrowserRouter as Router, Link, Route, Redirect, Switch} from 'react-router-dom'
 import Navbar from './components/navbar.jsx'
 import Login from './components/login.jsx'
+import Signup from './components/signup.jsx'
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state ={
-            loggedIn: true
+            loggedIn: false,
+            user: ''
         }
+        this.handleSignup = this.handleSignup.bind(this)
     }
 
 
     //Method section
-
+    handleSignup(newUsername, NewPassword) {
+        axios.post('/pets/signup', {username: newUsername, password: NewPassword})
+             .then((response) => {
+                 console.log(response.data)
+                 this.setState({user: newUsername, loggedIn: true})
+             })
+             .catch((err) => console.error(err))
+    }
 
 
     //Render Section
@@ -27,7 +37,8 @@ class App extends React.Component {
                     we're on the page!
                     <Switch>
                         <Route exact path='/' render = {() => <Redirect to='/login' />} />
-                        <Route path='/login' component = {Login} />
+                        <Route path='/login' render = {() => <Login />} />
+                        <Route path='/signup' render = {() => <Signup signup = {this.handleSignup} />} />
                     </Switch>
                 </div>
             </Router>
