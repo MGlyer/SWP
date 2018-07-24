2f2c9f41e0b863a7e76a185470a733873773ed78
Martin <MartinGlyer@gmail.com>
2018-07-24 14:54:27 -0400

    first attempt at writign a firebase db method

--- database/index.js
+++ database/index.js
@@ -1,1 +1,21 @@
 const db = require('./connection')
+
+const saveNewUser = async (username, password, cb) => {
+    let uniqueTest = await db.ref('/users' + username)
+    if (uniqueTest) {
+        //send back fail info
+    } else {
+        db.ref('/users' + username).set({
+            username: username,
+            password, password
+        }, (error) => {
+            if (error) cb(error)
+            else cb(null)
+        })
+    }
+}
+
+
+module.exports = {
+    saveNewUser
+}
