e0a46bf3a42dd2f3f7820fb39762d0e14258b307
Martin <MartinGlyer@gmail.com>
2018-07-24 10:39:11 -0400

    adds Firebase to index.html

--- server/contollers.js
+++ server/contollers.js
@@ -1,3 +1,18 @@
 module.exports = {
     //write out GETS and POSTS here
+    signup: {
+        post: (req, res) => {}
+    },
+
+    login: {
+        get: (req, res) => {}
+    },
+
+    more: {
+        get: (req, res) => {}
+    },
+
+    swipeRight: {
+        post: (req, res) => {}
+    }
 }
