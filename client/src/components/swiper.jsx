4222b86818ec86197b1fa4878b07154efbe762cc
Martin <MartinGlyer@gmail.com>
2018-07-25 13:05:13 -0400

    sets up more pets on swiper page

--- client/src/components/swiper.jsx
+++ client/src/components/swiper.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
+import axios from 'axios'
 
 class Swiper extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            //some state
+            listOfAnimals: [],
+            zipcode: '10031'
         }
+        this.getMorePets = this.getMorePets.bind(this)
+        this.nextPet = this.nextPet.bind(this)
     }
 
-    //Method section
 
+    //Method section
+    getMorePets() {
+        axios.get('/pets/more', {params: {zipcode: this.state.zipcode}})
+             .then((response) => {
+                 let currentAnimals = this.state.listOfAnimals.slice();
+                 let updatedAnimals = currentAnimals.concat(response.data);
+                 this.setState({listOfAnimals: updatedAnimals})
+
+             })
+             .catch((err) => console.error(err))
+    }
+
+    nextPet() {
+        let updatedAnimals = this.state.listOfAnimals.slice(1)
+        this.setState({listOfAnimals: updatedAnimals}, () => {
+            if (updatedAnimals.length < 2) this.getMorePets()
+        })
+    }
 
     render() {
         return(
@@ -17,8 +38,9 @@ class Swiper extends React.Component {
                 <div className='petContainer'>
                     <span>some info about the pet</span>
                 </div>
-                <button>Swipe Left</button>
+                <button onClick = {this.nextPet} >Swipe Left</button>
                 <button>Swipe Right</button>
+                <button onClick = {this.getMorePets}>More Life</button>
             </div>
         )
     }
